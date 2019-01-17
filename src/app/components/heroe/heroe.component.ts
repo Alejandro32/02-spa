e5f413c94e4896d73b0ab54dde0367a081d98088
Alejandro32 <32531783+Alejandro32@users.noreply.github.com>
2019-01-17 17:24:06 -0600

    test

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor( private ativatedRoute: ActivatedRoute ) {
+    this.ativatedRoute.params.subscribe(params => {console.log ( params );
+    })
   }
 
 }
